23b51fcf5406ed6eb2911265f901cc5eab252e7b
Cuppyzh <yapaditama@gmail.com>
2022-07-22 17:12:43 +0700

    Re-Init project

--- main.go
+++ main.go
@@ -1,41 +1,12 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
+	diako "github.com/cuppyzh/Go-Diako"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	router := setupRouter()
-	router.Run(":9008")
-}
-
-func setupRouter() *gin.Engine {
 	router := gin.Default()
-
-	// Ping test API
-	router.GET("/api/test/ping", func(context *gin.Context) {
-		context.String(http.StatusOK, "pong")
-	})
-
-	// Diako
-	router.POST("/api/diako/message", func(context *gin.Context) {
-		var request MessageRequest
-
-		if err := context.ShouldBindJSON(&request); err != nil {
-			context.JSON(http.StatusBadRequest, errorResponse(err))
-			return
-		}
-
-		log.Println("Test: " + request.Sender)
-		context.String(http.StatusOK, "")
-	})
-
-	return router
-}
-
-func errorResponse(err error) gin.H {
-	return gin.H{"error": err.Error()}
+	diako.SetupRouter(router)
+	router.Run()
 }
--- models.go
+++
@@ -1,6 +0,0 @@
-package main
-
-type MessageRequest struct {
-	Sender 	string
-	Message	string
-}
